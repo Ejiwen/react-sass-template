ebacc974cf29400f150f0597886fe27200daa999
Ejiwen <skibnewrim@gmail.com>
2021-10-20 00:09:49 -0700

    delete the server folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,48 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Layout from "./layouts/Layout";
-import AddTask from "./AddTask.js";
-import Tasks from "./Tasks.js";
 
-const App = () => {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    getTasks();
-  }, [tasks]);
-
-  const saveTask = (task) => {
-    axios.post("/task", {
-      task: task,
-    });
-    getTasks();
-  };
-
-  const deleteTask = (taskId) => {
-    axios.post("/remove", { id: taskId }).then((res) => console.log(res.data));
-  };
-
-  const editTask = (id, taskup) => {
-    axios
-      .post("/edit", { id: id, taskup: taskup })
-      .then((res) => console.log(res.data));
-  };
-
-  const getTasks = () => {
-    axios.get("/tasks").then((res) => setTasks(res.data));
-  };
-
-  const obj = {
-    deleteTask,
-    editTask,
-  };
-
-  return (
-    <Layout>
-      <AddTask saveTask={saveTask} />
-      <Tasks tasks={tasks} obj={obj} />
-    </Layout>
-  );
-};
+const App = () => (
+  <h1>Hello from App ...</h1>
+)
 
 export default App;
